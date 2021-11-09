db8edbd60df63cce23f68f39ee9ce40d1d165c0f
Atria <atriasft@gmail.com>
2021-11-10 02:56:04 +0900

    implementention scroll_zoom(PC)

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let scale = 1;
+window.addEventListener('wheel', function(event){
+    const yuhi = this.document.getElementById('yuhi');
+    scale += event.deltaY * -0.001;
+    yuhi.style.transform = `scale(${scale})`;
+});
+
+function handleTouchMove(event) {
+    event.preventDefault();
+}
+//スクロールの禁止
+document.addEventListener('wheel', handleTouchMove, { passive: false });
