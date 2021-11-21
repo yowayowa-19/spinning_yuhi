4541cbef59e3cc7f274b56236f67728d29a69d72
yuhi-dosei <yuhi.dosei@gmail.com>
2021-11-22 04:29:45 +0900

    [WIP] touchmoveの処理書いた

--- main.js
+++ main.js
@@ -6,11 +6,21 @@ window.addEventListener('wheel', function(event){
     yuhi.style.transform = `scale(${scale})`;
 });
 
+window.addEventListener('touchmove', function(event){
+    const yuhi = this.document.getElementById('yuhi');
+    scale += event.deltaY * -0.001;
+    yuhi.style.transform = `scale(${scale})`;
+});
+
+
 function handleTouchMove(event) {
     event.preventDefault();
 }
+
 //スクロールの禁止
 document.addEventListener('wheel', handleTouchMove, { passive: false });
+
+document.addEventListener('touchmove', handleTouchMove, { passive: false });
 
 window.onload = function () { 
     const profileDiv = document.getElementById("profile");
